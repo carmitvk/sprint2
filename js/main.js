3c9f202bd59f182fbcab7b7f2cb18759d9f91c2d
carmit vaknin <carmit.vk@gmail.com>
2021-02-10 17:51:55 +0200

    gallery pictures

--- js/main.js
+++ js/main.js
@@ -1,8 +1,81 @@
 
 'use strict'
+var gElCanvas;
+var gCtx;
+
 
 function init() {
     gElCanvas = document.getElementById('meme-canvas');
     gCtx = gElCanvas.getContext('2d');
-    console.log('init -> gCtx', gCtx);
+    renderGallery();
 }
+
+function onNavClicked(el, clsName) {
+    var elements = document.querySelectorAll('.sub-nav-press.sub-nav');
+    elements.forEach(element => {
+        element.classList.toggle('sub-nav-press');
+    }); 
+    el.classList.toggle('sub-nav-press');
+
+    elements = document.querySelectorAll('section');
+    elements.forEach(element => {
+        element.classList.add('hide');
+    })
+    var element = document.querySelector('.'+clsName);
+    element.classList.remove('hide');
+}
+
+
+function drawText(text, x, y) {
+    gCtx.lineWidth = 2
+    gCtx.strokeStyle = 'black'
+    gCtx.fillStyle = gColor
+    gCtx.font = '40px Arial'
+    gCtx.textAlign = 'center'
+    gCtx.fillText(text, x, y)
+    gCtx.strokeText(text, x, y)
+}
+
+function renderGallery(){
+    var imgs = getImgs();
+    var strHtml = imgs.map(img => {
+                                    return `<img class='one-img' src="./img/${img.id}.jpg" onclick='onPickImg(${img.id})' alt="">`
+                                  });
+
+    var elGallery = document.querySelector('.gallery-container');
+    elGallery.innerHTML = strHtml.join('');
+}
+
+function onPickImg(id){
+    //put into canvas
+    document.querySelector('canvas').innerHTML = ''
+    // var reader = new FileReader();
+
+    // reader.onload = function (event) {
+        var img = new Image()
+        img.src = `img/${id}.jpg`; //event.target.result
+    //     img.onload = onImageReady.bind(null, img)
+    //     // gImg = img
+    // }
+    // console.log(ev.target.files[0]);
+    // reader.readAsDataURL(ev.target.files[0])
+
+    gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+
+
+
+
+
+
+    //hide gallery
+    var element = document.querySelector('.gallery-container');
+    element.classList.add('hide');
+
+    //display the Editor
+    var element = document.querySelector('.editor-container');
+    element.classList.remove('hide');
+
+    //Save to current Meme
+
+}
+
--- js/upload_service.js
+++ js/upload_service.js
@@ -32,3 +32,43 @@ function doUploadImg(elForm, onSuccess) {
     })
 }
 
+
+
+// The next 2 functions handle IMAGE UPLOADING to img tag from file system: 
+function onImgInput(ev) {
+    loadImageFromInput(ev, renderImg)
+}
+
+// function loadImageFromInput(ev, onImageReady) {
+//     document.querySelector('canvas').innerHTML = ''
+//     var reader = new FileReader()
+
+//     reader.onload = function (event) {
+//         var img = new Image()
+//         img.onload = onImageReady.bind(null, img)
+//         img.src = event.target.result
+//         // gImg = img
+//     }
+//     reader.readAsDataURL(ev.target.files[0])
+// }
+
+function loadImageFromInput(ev, onImageReady) {
+    document.querySelector('canvas').innerHTML = ''
+    var reader = new FileReader();
+
+    reader.onload = function (event) {
+        var img = new Image()
+        img.src = 'img/2.jpg'; //event.target.result
+        img.onload = onImageReady.bind(null, img)
+        // gImg = img
+    }
+    console.log(ev.target.files[0]);
+    reader.readAsDataURL(ev.target.files[0])
+}
+
+
+
+
+// function renderImg(img) {
+//     gCtx.drawImage(img, 0, 0, gElCanvas.width, gElCanvas.height);
+// }
